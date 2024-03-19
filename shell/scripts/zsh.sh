5a7cfdf11c1ae83e5efedf49499dbe13dc4336c5
tzvonimir <zvonimir.tomesic@gmail.com>
2024-03-19 19:57:00 +0100

    Restructure command collection mechanism

--- shell/scripts/zsh.sh
+++ shell/scripts/zsh.sh
@@ -5,14 +5,15 @@ if command -v gdate > /dev/null; then
 fi
 
 preexec() {
-    # Capture the command start time in milliseconds
     export COMMAND_START_TIME=$($DATE_CMD +%s%3N)
     export LAST_COMMAND=$1
+    # Send a start execution message
+    {{.CommandScriptPath}} "start" "$LAST_COMMAND" "$PWD" "$USER" "$COMMAND_START_TIME"
 }
 
 precmd() {
     local end_time=$($DATE_CMD +%s%3N)
     local duration=$((end_time - COMMAND_START_TIME))
-    # Call the logging script with command execution details
-    {{.CommandScriptPath}} "$LAST_COMMAND" "$PWD" "$USER" "$COMMAND_START_TIME" "$end_time" "$duration"
+    # Send an end execution message
+    {{.CommandScriptPath}} "end" "$LAST_COMMAND" "$PWD" "$USER" "$COMMAND_START_TIME" "$end_time" "$duration"
 }
